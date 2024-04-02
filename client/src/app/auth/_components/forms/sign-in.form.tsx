1d1aa973c391ee2f9e19224b10c228a6d058c5fd
rambutan <pride.kraievskyi@gmail.com>
2024-04-02 16:02:05 +0300

    add schemas && pages

---
+++ client/src/app/auth/_components/forms/sign-in.form.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+export default function SignInForm() {
+	return(
+		<div>SignInForm</div>
+	)
+}
--- client/src/types/forms/index.tsx
+++ client/src/types/forms/index.tsx
@@ -4,6 +4,7 @@ import { validateCode } from "@/lib/utils";
 export const JoinMeetingValidationSchema = z.object({
   code: z.string().refine(validateCode, "Invalid code"),
 });
+
 export const CreateMeetingValidationSchema = z.object({
   name: z
     .string()
@@ -11,5 +12,18 @@ export const CreateMeetingValidationSchema = z.object({
     .min(2, "Min length is 2 chars"),
 });
 
+export const SignInValidationSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(5, "Min length is 5 chars").max(20, "Max length is 20 chars")
+});
+
+export const SignUpValidationSchema = z.object({
+  email: z.string().email("Invalid email"),
+  name: z.string().min(1, "Name is required").max(100, "Max length is 100 chars"),
+  password: z.string().min(5, "Min length is 5 chars").max(20, "Max length is 20 chars")
+});
+
+export type SignInInputs = z.infer<typeof SignInValidationSchema>;
+export type SignUpInputs = z.infer<typeof SignUpValidationSchema>;
 export type JoinMeetingInputs = z.infer<typeof JoinMeetingValidationSchema>;
 export type CreateMeetingInputs = z.infer<typeof CreateMeetingValidationSchema>;
